587f160839ddb82f05f39ab42cc5ebc0f1260f8d
Reversi Steem <reversteem@hotmail.com>
2022-10-16 02:32:25 +0900

    Modified steem_keychain

--- js/keychain.js
+++ js/keychain.js
@@ -1,14 +1,16 @@
 "use strict";
 
-if (window.steem_keychain) {
-  console.log('Steem Keychain extension installed...');
-  try {
-    steem_keychain.requestHandshake(function() {
-      console.log('Handshake received!');
-    });
-  } catch (e) {
-    console.log(e);
+const checkKeychain = () => {
+  if (window.steem_keychain) {
+    console.log('Steem Keychain extension installed...');
+    try {
+      steem_keychain.requestHandshake(function() {
+	console.log('Handshake received!');
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    console.log('Steem Keychain extension not installed...');
   }
-} else {
-  console.log('Steem Keychain extension not installed...');
 }
